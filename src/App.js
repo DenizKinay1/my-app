620ddfb4d4a7535477d935d7d63e9e4f1debf885
Deniz Kınay <denizkinay1@gmail.com>
2021-07-16 12:16:36 +0300

    commit16-07

--- src/App.js
+++ src/App.js
@@ -173,7 +173,7 @@ function App() {
                 nulla pariatur. Excepteur sint occaecat cupidatat non proident,
                 sunt in culpa qui officia deserunt mollit anim id est laborum.
               </p>
-              <Button color="primary" onClick={handleLogin}>GET STARTED</Button>
+              
             </div>
             
           </div>
